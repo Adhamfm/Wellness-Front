ab3d249db7da89d8e76752cdb256018c1feadb49
AhmedMohamed50 <85414834+AhmedMohamed50@users.noreply.github.com>
2024-03-19 00:49:47 +0200

    Update index.jsx

--- src/pages/mealPage/index.jsx
+++ src/pages/mealPage/index.jsx
@@ -17,16 +17,31 @@ export default function MealPage() {
         //setTimeout(() => setIsLoading(false), 1000);
     
         setIsLoading(false)
-      }
+    }
     console.log(meal)
     return (
         <>
             <NavBar />
-            <h1>TITLE: {meal.title}</h1>
-            {meal.images ? <img src={meal.images[2]} alt="Meal Image"/> : null}
-            <h4>PRICE: {meal.price}</h4>
-            <p>DESCRIPTION: {meal.description}</p>
-            
+            <div style={styles.poster}>
+                <h1>TITLE: {meal.title}</h1>
+                {meal.images ? <img style={{border: "5px solid white", width: "100%"}} src={meal.images[2]} alt="Meal Image"/> : null}
+                <h4>PRICE: {meal.price}</h4>
+                <p>DESCRIPTION: {meal.description}</p>
+            </div>
+            <Footer />
         </>
     )
 }
+
+// CSS styles
+const styles = {
+    poster: {
+        width: "50%",
+        marginLeft: "25%",
+        marginTop: "50px",
+        fontFamily: "Libre Baskerville, serif",
+        textAlign: "center",
+        marginBottom: "100px",
+    },
+};
+
