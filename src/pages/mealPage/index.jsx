bddd5bacfcd5730bfe08133308bf6b68a450d2d3
AhmedMohamed50 <85414834+AhmedMohamed50@users.noreply.github.com>
2024-03-19 15:23:15 +0200

    Update index.jsx

--- src/pages/mealPage/index.jsx
+++ src/pages/mealPage/index.jsx
@@ -3,34 +3,43 @@ import Footer from "../../components/layout/Footer/Footer";
 import NavBar from "../../components/layout/NavBar/NavBar";
 import useMealPage from "./useMealPage";
 
-export default function MealPage() {
+import Button from '@mui/material/Button';
 
+export default function MealPage() {
     const { 
         meal,
         mealId,
         selectedQuantity,
         handleQuantityChange,
-    } = useMealPage()
+    } = useMealPage();
 
-    function onLoad() {
-        // delay for demo only
-        //setTimeout(() => setIsLoading(false), 1000);
-    
-        setIsLoading(false)
-    }
-    console.log(meal)
+    const [isAddedToCart, setIsAddedToCart] = useState(false);
+
+    // Function to handle adding the meal to the cart
+    const addToCart = () => {
+        setIsAddedToCart(true);
+    };
+
+    console.log(meal);
+
     return (
         <>
             <NavBar />
             <div style={styles.poster}>
                 <h1>TITLE: {meal.title}</h1>
-                {meal.images ? <img style={{border: "5px solid white", width: "100%"}} src={meal.images[2]} alt="Meal Image"/> : null}
+                {meal.images && <img style={{border: "5px solid white", width: "100%"}} src={meal.images[2]} alt="Meal Image" />}
                 <h4>PRICE: {meal.price}</h4>
                 <p>DESCRIPTION: {meal.description}</p>
+                <div style={styles.button}>
+                    {/* Render the button only if the meal hasn't been added to the cart */}
+                    {!isAddedToCart && <Button variant="contained" onClick={addToCart}>Add to Cart</Button>}
+                    {/* Show a message if the meal has been added to the cart */}
+                    {isAddedToCart && <p>Added to Cart</p>}
+                </div>
             </div>
             <Footer />
         </>
-    )
+    );
 }
 
 // CSS styles
@@ -42,6 +51,9 @@ const styles = {
         fontFamily: "Libre Baskerville, serif",
         textAlign: "center",
         marginBottom: "100px",
+        fontSize: "25px",
     },
+    button: {
+        marginTop: "40px",
+    }
 };
-
