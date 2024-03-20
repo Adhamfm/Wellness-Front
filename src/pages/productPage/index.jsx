c0593adb56883bd306879ba9dc7dc5068cbb659a
AhmedMohamed50 <85414834+AhmedMohamed50@users.noreply.github.com>
2024-03-21 00:03:27 +0200

    productPage & profile CSS style

--- src/pages/productPage/index.jsx
+++ src/pages/productPage/index.jsx
@@ -16,11 +16,29 @@ export default function ProductPage() {
     return (
         <>
             <NavBar />
-            <h1>ProductPage</h1>
-            <h2>TITLE: {product.title}</h2>
-            <h3>PRICE: {product.price}</h3>
-            <p>DESCRIPTION:{product.description}</p>
+            <div style={styles.poster}>
+                <h1>ProductPage</h1>
+                <h2>TITLE: {product.title}</h2>
+                <h3>PRICE: {product.price}</h3>
+                <p style={{marginTop: "30px"}}>DESCRIPTION:{product.description}</p>
+            </div>
             <Footer />
         </>
     )
 }
+
+// CSS styles
+const styles = {
+    poster: {
+        width: "50%",
+        marginLeft: "25%",
+        marginTop: "50px",
+        fontFamily: "Libre Baskerville, serif",
+        // textAlign: "center",
+        marginBottom: "100px",
+        fontSize: "25px",
+    },
+    btn: {
+        marginTop: "40px",
+    }
+};
