0b72d5062ca1b546e2b091a9e01061542d7d9ba5
AhmedMohamed50 <85414834+AhmedMohamed50@users.noreply.github.com>
2024-03-20 21:54:04 +0200

    Update index.jsx

--- src/pages/mealPage/index.jsx
+++ src/pages/mealPage/index.jsx
@@ -17,19 +17,38 @@ export default function MealPage() {
         //setTimeout(() => setIsLoading(false), 1000);
     
         setIsLoading(false)
-      }
+    }
     console.log(meal)
     
     return (
         <>
             <NavBar />
-            <h1>TITLE: {meal.title}</h1>
-            {meal.images ? <img src={meal.images[2]} alt="Meal Image"/> : null}
-            <h4>PRICE: {meal.price}</h4>
-            <p>DESCRIPTION: {meal.description}</p>
-            <input type="number" value={selectedQuantity} onChange={handleQuantityChange} />
-            <h4>Total:{meal.price * selectedQuantity} EGP</h4>
-            <button className="btn btn-primary">ADD TO CART</button>
+            <div style={styles.poster}>
+                <h1>TITLE: {meal.title}</h1>
+                {meal.images ? <img style={{border: "5px solid white", width: "100%"}} src={meal.images[2]} alt="Meal Image"/> : null}
+                <h4>PRICE: {meal.price}</h4>
+                <p>DESCRIPTION: {meal.description}</p>
+                <input type="number" value={selectedQuantity} onChange={handleQuantityChange} />
+                <h4>Total:{meal.price * selectedQuantity} EGP</h4>
+                <button className="btn btn-primary">ADD TO CART</button>
+            </div>
+            <Footer />
         </>
     )
 }
+
+// CSS styles
+const styles = {
+    poster: {
+        width: "50%",
+        marginLeft: "25%",
+        marginTop: "50px",
+        fontFamily: "Libre Baskerville, serif",
+        textAlign: "center",
+        marginBottom: "100px",
+        fontSize: "25px",
+    },
+    btn: {
+        marginTop: "40px",
+    }
+};
