bc892ac9097fa54bbde0476a2560839a282efca6
Adhamfm <57398840+Adhamfm@users.noreply.github.com>
2024-06-19 19:55:07 +0300

    added to cart notification in product and meals page

--- src/components/mealCard/index.jsx
+++ src/components/mealCard/index.jsx
@@ -4,9 +4,12 @@ import React, { useState, useRef, useEffect } from 'react'
 import Wishlist from './wishlist';
 import wellnessImg from '/assets/WellnessLogo.png'
 import axios from 'axios';
+import { Alert, IconButton, Snackbar } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function MealCard(props) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [open, setOpen] = useState(false);
   useEffect(() => {
     function checkLoggedIn() {
       const user = localStorage.getItem('user') !== null
@@ -34,10 +37,36 @@ export default function MealCard(props) {
         quantity: 1
       }, { headers: { "authorization": `Bearer ${userLocal.accessToken}` } });
       console.log(response);
+      handleOpen()
     } catch (error) {
       console.log(error);
     }
   }
+  const handleOpen = () => {
+    setOpen(true);
+};
+
+const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+        return;
+    }
+
+    setOpen(false);
+};
+
+const action = (
+    <>
+        <IconButton
+            size="small"
+            aria-label="close"
+            color="inherit"
+            onClick={handleClose}
+        >
+            <CloseIcon fontSize="large" />
+        </IconButton>
+    </>
+);
+
   //console.log(localStorage.getItem("inputValue"),props.data.title)
   const [isLoading, setIsLoading] = useState(true);
   const [imgSrc, setImgSrc] = useState("\assets\WellnessLogo.png")
@@ -56,6 +85,25 @@ export default function MealCard(props) {
       </Link>
       <span>MEAL</span>
       <h4>{props.data.title}</h4>
+      <Snackbar
+
+        open={open}
+        autoHideDuration={2000}
+        onClose={handleClose}
+        variant="success"
+
+        action={action}
+      >
+        <Alert
+          onClose={handleClose}
+          severity="success"
+          variant="filled"
+          sx={{ width: '100%' }}
+          style={{ fontSize: "16px" }}
+        >
+          Added to Cart
+        </Alert>
+      </Snackbar>
       <div className="stars">
         <i className="fa-solid fa-star"></i>
         <span><span className="specific">{props.data.rate} </span>(630)</span>
@@ -67,7 +115,7 @@ export default function MealCard(props) {
         </>
       ) : (
         <>
-           <Link to="/logincustomer"> <i className="fa-solid fa-cart-shopping buy-icon" onClick={handleClick}></i></Link>
+          <Link to="/logincustomer"> <i className="fa-solid fa-cart-shopping buy-icon" onClick={handleClick}></i></Link>
         </>
       )}
 
--- src/components/productCard/index.jsx
+++ src/components/productCard/index.jsx
@@ -4,11 +4,14 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 import wellnessImg from '/assets/WellnessLogo.png'
 import Slider from '../Slider/Slider'
+import { Alert, IconButton, Snackbar } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 //import MuiImageSlider from 'mui-image-slider';
 
 
 export default function ProductCard(props) {
+  const [open, setOpen] = useState(false);
   // if (props.id){
   //   (async () => {
   //     try {
@@ -47,34 +50,80 @@ export default function ProductCard(props) {
         itemId: props.data._id,
         quantity: 1
       }, { headers: { "authorization": `Bearer ${userLocal.accessToken}` } });
-      console.log(response);
+      console.log(response)
+      handleOpen()
     } catch (error) {
       console.log(error);
     }
   }
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const action = (
+    <>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="large" />
+      </IconButton>
+    </>
+  );
+
   return (
     <div className="product-cart">
       {/* {console.log(props)} */}
-        {/* <img src={props.data.images[0]} alt="product image" /> */}
-        <Slider images={props.data.images}/>
+      {/* <img src={props.data.images[0]} alt="product image" /> */}
+      <Slider images={props.data.images} />
       <Link to={`/products/${props.data._id}`} style={{ textDecoration: 'none' }} >
         <span>SHOP</span>
-      <h4>{props.data.title}</h4>
-      <div className="stars">
-        <i className="fa-solid fa-star"></i>
-        <span><span className="specific">{props.data.finalrate} </span>(630)</span>
-      </div>
-      <h4 className="price">EGP {props.data.price}</h4>
+        <h4>{props.data.title}</h4>
+        <Snackbar
+
+          open={open}
+          autoHideDuration={2000}
+          onClose={handleClose}
+          variant="success"
+
+          action={action}
+        >
+          <Alert
+            onClose={handleClose}
+            severity="success"
+            variant="filled"
+            sx={{ width: '100%' }}
+            style={{ fontSize: "16px" }}
+          >
+            Added to Cart
+          </Alert>
+        </Snackbar>
+        <div className="stars">
+          <i className="fa-solid fa-star"></i>
+          <span><span className="specific">{props.data.finalrate} </span>(630)</span>
+        </div>
+        <h4 className="price">EGP {props.data.price}</h4>
+      </Link>
       {isLoggedIn ? (
         <>
           <i className="fa-solid fa-cart-shopping buy-icon" onClick={handleClick}></i>
         </>
       ) : (
         <>
-           <Link to="/logincustomer"> <i className="fa-solid fa-cart-shopping buy-icon" onClick={handleClick}></i></Link>
+          <Link to="/logincustomer"> <i className="fa-solid fa-cart-shopping buy-icon" onClick={handleClick}></i></Link>
         </>
       )}
-      </Link>
     </div>
   )
 }
